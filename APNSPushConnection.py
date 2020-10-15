a8fe29a570e15df7aecc0deffc02bcf639c9f749
Chris Coffin <me@chriscoffin.org>
2020-10-15 01:50:10 +0000

    Refactoring

--- APNSPushConnection.py
+++ APNSPushConnection.py
@@ -1,81 +1,95 @@
 #!/usr/bin/env python3
 
-import requests
+"""
+APNSPushSession.py
+https://github.com/magnolialogic/python-apns_server
+"""
+
 import sys
+from json import loads
+import requests
 import yaml
 from apns2.client import APNsClient
 from apns2.credentials import TokenCredentials
 from apns2.payload import Payload, PayloadAlert
-from json import loads
 
-class APNSPushSession:
-	def __init__(self, background=None, title="Title", body="Body", silent=True, badge=0, dev=True, yaml_tokens=False):
-		self.title = title
-		self.body = body
-		self.silent = silent
-		self.badge = badge
-		self.background = background
-		self.dev = dev
-		self.yaml_tokens = yaml_tokens
-		if silent:
-			self.sound = None
-		else:
-			self.sound = "Default"
+class APNSPushConnection:
+	"""
+	Create and push APNS notification to iOS devices
+	"""
 
-	def send(self):
-		# Get config data from YAML files
-		with open ("app.yaml") as app_file:
+	def __init__(self, sandbox=True, yaml_tokens=False):
+		self.sandbox = sandbox
+		self.yaml = yaml_tokens
+
+		self.payload = None
+		self.tokens = []
+
+		with open("app.yaml") as app_file:
 			try:
 				loaded_app = yaml.safe_load(app_file)
 			except yaml.YAMLError:
 				sys.exit(yaml.YAMLError)
+			else:
+				self.config = {
+					"auth-key": loaded_app["auth-key"],
+					"auth-key-filename": loaded_app["auth-key-filename"],
+					"bundle-id": loaded_app["bundle-id"],
+					"cert-filename": loaded_app["cert-filename"],
+					"team-id": loaded_app["team-id"],
+					"api-url": loaded_app["api-url"] + "/tokens/" + loaded_app["bundle-id"]
+				}
 
-		with open ("config.yaml") as config_file:
-			try:
-				config = yaml.safe_load(config_file)
-			except yaml.YAMLError:
-				sys.exit(yaml.YAMLError)
+		self.credentials = TokenCredentials(auth_key_path=self.config["auth-key-filename"], auth_key_id=self.config["auth-key"], team_id=self.config["team-id"])
+		self.client = APNsClient(credentials=self.credentials, use_sandbox=self.sandbox)
 
-		# Get tokens from YAML file or APNS remote server API depending on arguments
-		if self.yaml_tokens == True:
-			with open ("tokens.yaml") as token_file:
+		self.load_tokens()
+
+	def load_tokens(self):
+		"""
+		Loads tokens from remote notification server or YAML file
+		"""
+		if self.yaml:
+			with open("tokens.yaml") as token_file:
 				try:
-					loaded_tokens = [token for token in list(yaml.safe_load_all(token_file)) if not token == None]
-					if len(loaded_tokens) == 0:
-						sys.exit("No target device tokens in tokens.yaml")
-					tokens = [target["device-token"] for target in loaded_tokens if target["bundle-id"] == loaded_app["bundle-id"]]
-					if len(tokens) == 0:
-						sys.exit("No target device tokens with matching BundleID")
+					loaded_tokens = [token for token in list(yaml.safe_load_all(token_file)) if token is not None]
 				except yaml.YAMLError:
 					sys.exit(yaml.YAMLError)
+				else:
+					self.tokens = [target["device-token"] for target in loaded_tokens if target["bundle-id"] == self.config["bundle-id"]]
+					if len(self.tokens) == 0:
+						sys.exit("Invalid data: no device tokens defined with matching BundleID")
 		else:
-			api_url = config["api-url"] + "/tokens/" + loaded_app["bundle-id"]
 			try:
-				tokens = list(loads(requests.get(api_url).text))
-			except:
-				sys.exit("Unable to parse JSON response, check APNS remote notification server.")
+				self.tokens = list(loads(requests.get(self.config["api-url"]).text))
+			except: # pylint: disable=W0702
+				sys.exit("Invalid data: no device tokens in JSON response, check remote notification server.")
 
-		# Assign loaded .yaml data to constants
-		auth_key = loaded_app["auth-key"]
-		auth_key_filename = loaded_app["auth-key-filename"]
-		bundle_id = loaded_app["bundle-id"]
-		cert_filename = loaded_app["cert-filename"]
-		team_id = loaded_app["team-id"]
-
-		# Create payload based on notification type
-		if self.background:
-			data = {"Data": self.background}
-			payload = Payload(content_available=True, custom=data)
+	def create_payload(self, background=None, title="Title", body="Body", silent=True, badge=0): # pylint: disable=R0913
+		"""
+		Constructs APNS payload
+		"""
+		if silent:
+			sound = None
 		else:
-			alert = PayloadAlert(title=self.title, body=self.body)
-			payload = Payload(alert=alert, sound=self.sound, badge=self.badge)
+			sound = "Default"
+		if background is not None:
+			data = {"Data": background}
+			self.payload = Payload(content_available=True, custom=data)
+		else:
+			alert = PayloadAlert(title=title, body=body)
+			self.payload = Payload(alert=alert, sound=sound, badge=badge)
 
-		# Create session and post notification
-		credentials = TokenCredentials(auth_key_path=auth_key_filename, auth_key_id=auth_key, team_id=team_id)
-		client = APNsClient(credentials=credentials, use_sandbox=self.dev)
-		for target in tokens:
-			print("Sending APNS payload", payload.dict(), "to", target)
-			client.send_notification(token_hex=target, notification=payload, topic=bundle_id)
+	def push(self):
+		"""
+		Sends APNS notification to given device IDs
+		"""
+		if not None in self.__dict__.values() and len(self.tokens) > 0:
+			for target in self.tokens:
+				print("Sending APNS payload", self.payload.dict(), "to", target)
+				self.client.send_notification(token_hex=target, notification=self.payload, topic=self.config["bundle-id"])
+		else:
+			sys.exit("Invalid data: failed to send notification, check configuration data.")
 
 if __name__ == "__main__":
-	sys.exit("I am a module, not a script.")
+	sys.exit("I am not a script.")
